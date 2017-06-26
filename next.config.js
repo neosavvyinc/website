83f1768cec3fa39643fa4269b4e474df7a338224
Pablo Alonso <pabloalonsos.eu@gmail.com>
2017-06-26 15:50:35 -0400

    deployment

---
+++ next.config.js
@@ -0,0 +1,11 @@
+exports.exportPathMap = () => ({
+  "/": { page: "/" },
+  "/about": { page: "/about" },
+  "/careers": { page: "/careers" },
+  "/contact": { page: "/contact" },
+  "/engineer": { page: "/engineer" },
+  "/lead_engineer": { page: "/lead_engineer" },
+  "/services": { page: "/services" },
+  "/ux": { page: "/ux" },
+  "/ux_lead": { page: "/ux_lead" }
+})
