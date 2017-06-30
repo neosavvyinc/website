3473578fc2d6e80de9341646c6ed74b5b1feba02
Pablo Alonso <pabloalonsos.eu@gmail.com>
2017-06-29 20:40:22 -0400

    case studies responsive

--- components/case_studies.component.js
+++ components/case_studies.component.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import sizeMe from 'react-sizeme';
 
 const companies = [
   {
@@ -33,57 +34,103 @@ const companies = [
   },
 ];
 
-export default () => (
-  <div style={styles.container}>
-    <div className="card" style={{ maxWidth: '80vh' }}>
-      <div className="card-content" style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        <p className="title is-4">
-          Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.
-        </p>
-        <p className="subtitle">- Albert Einstein</p>
-      </div>
-    </div>
-    {
-      _.map(companies, company => (
-        <div
-          key={company.name}
-          className="card"
-          style={styles.card}
-        >
-          <div className="card-content" style={{height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-            <p/>
-            <p className="title">
-              <img
-                src={company.logo}
-                alt={company.name}
-              />
-            </p>
-            <p className="subtitle">
-              {company.name}
-            </p>
-          </div>
-        </div>
-      ))
-    }
-  </div>
-);
+const CaseStudies = ({ size }) => {
+  const isMobile = size.width < 768;
+  const isTablet = size.width < 1220;
+  const styles = createStyles(isMobile, isTablet);
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center'
-  },
-  card: {
-    width: '300px'
-  },
-  caseContainer: {
-    width: '240px',
-  },
-  caseImage: {
-    //    height: '240px',
-    //    margin: '12px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  return (
+    <div style={styles.container}>
+      <div className="card" style={styles.quoteContainer}>
+        <div className="card-content" style={styles.quoteContent}>
+          <p className="title is-4">
+            Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.
+          </p>
+          <p className="subtitle">- Albert Einstein</p>
+        </div>
+      </div>
+      <span style={styles.cases}>
+        {
+          _.map(companies, company => (
+            <div
+              key={company.name}
+              className="card"
+              style={styles.card}
+            >
+              <div className="card-content" style={styles.cardContent}>
+                <p/>
+                <p className="title">
+                  <img
+                    src={company.logo}
+                    alt={company.name}
+                  />
+                </p>
+                <p className="subtitle">
+                  {company.name}
+                </p>
+              </div>
+            </div>
+          ))
+        }
+      </span>
+    </div>
+  );
+}
+
+export default sizeMe()(CaseStudies);
+
+const createStyles = (isMobile, isTablet) => {
+  const defaults = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    quoteContainer: {
+      maxWidth: '80vh'
+    },
+    quoteContent: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center'
+    },
+    card: {
+      width: '300px'
+    },
+    cardContent: {
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between'
+    },
+    caseContainer: {
+      width: '240px',
+    },
+    caseImage: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+  };
+
+  const tablet = isTablet && !isMobile ? {
+    container: {
+      display: 'block',
+      width: '100%'
+    },
+    quoteContainer: {
+      width: '100%',
+      maxWidth: 'auto'
+    },
+    cases: {
+      display: 'flex',
+      flexDirection: 'row'
+    },
+    card: {
+      //width: '100%'
+    }
+  } : {};
+  const mobile = isMobile ? {} : {};
+
+  return _.merge(defaults, tablet, mobile);
 }
